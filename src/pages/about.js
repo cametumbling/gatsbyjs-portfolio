881bf32500dfbd97d174a1553d68e6ea85f4f7a2
Jill Marcum <jill.marcum@gmail.com>
2021-04-01 02:00:26 -0700

    revise about page spacing

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,11 +12,14 @@ export default function About({ data }) {
         <Header headerText={data.site.siteMetadata.description}/>
         {/* <Header headerText="About me" /> */}
         <p style={{ color: `#ccc`}}>      
-        Hi! I'm Jill, a JavaScript-based web developer from California, working with technologies such as React, Node.js, Express, and PostgreSQL. (This site was built using Gatsby!) In a previous life, I was a GIS Data Analyst, so I'm great with data and I love maps--analog or digital! 
+        Hi! I'm Jill, a JavaScript-based web developer from California, working with technologies such as React, Node.js, Express, and PostgreSQL. (This site was built using Gatsby!) In a previous life, I was a GIS Data Analyst, so I'm great with data and I love maps! 
+        <br/>
         <br/>
         I hold two masters degrees, one in Anthropology/Archaeology from CUNY Hunter in New York, and the second in Cuneiform Studies from the University of Oxford in England. At Oxford, I organized several conferences, served as a cross-departmental liaison and social chair, acted as the webmaster for my boat club (go Wolfson!), volunteered for a trauma helpline, and was involved with Oxford Women of Computer Science. 
         <br/>
+        <br/>
         My last permanent role was at the Metropolitan Museum of Art in New York, where I got my start in front-end web development and Agile project management, joining the dev team for UX work, testing, and making responsive sites.
+        <br />
         <br />
         Most recently I have been working on an app to bring social justice to historically non-diverse spaces in the educational system.
       </p>
